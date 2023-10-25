83e1526e589de0ce06fc75d0281d9d9bdeaa3b47
woojung <dagam1240@gmail.com>
2023-10-25 15:19:13 +0900

    feat : setup project

---
+++ src/main/java/com/example/sw_week/SwWeekApplication.java
@@ -0,0 +1,13 @@
+package com.example.sw_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sw_week/SwWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sw_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
